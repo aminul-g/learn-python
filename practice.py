c3fb734fa1eb75a648e58a8676af13058b3986ea
aminul-a <md.aminulz01239@gmail.com>
2024-06-21 19:33:24 +0600

    initial commit

---
+++ practice.py
@@ -0,0 +1,4 @@
+roll = [101,102,103,104]
+name = ["amin", "hadi","aminul","hadiul"]
+z = list(zip(roll,name))
+print(z)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+t = int(input())
+while t > 0:
+    a, b = map(int,input().split())
+    sum = a+b
+    print(sum)
+    t -= 1
