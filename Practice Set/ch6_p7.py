1801c997b2c444df1d903cd62e3f7c14b16388f2
aminul-a <md.aminulz01239@gmail.com>
2024-06-23 09:37:08 +0600

    update ch6_p7

--- Practice Set/ch6_p7.py
+++ Practice Set/ch6_p7.py
@@ -1,6 +1,6 @@
 post = input("Enter the post content: ")
 name = "Aminul"
-if name in post:
+if name.lower() in post.lower():
     print(f"talking about {name}")
 else:
     print(f"not about {name}")
